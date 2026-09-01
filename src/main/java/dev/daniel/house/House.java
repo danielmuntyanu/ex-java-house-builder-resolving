87166806d1ae7e1ebf6bd57726a4951b40b38e14
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-01 11:01:05 +0200

    feat: House entity - added constructor, setters for optional attributes and getters for all attributes

--- src/main/java/dev/daniel/house/House.java
+++ src/main/java/dev/daniel/house/House.java
@@ -5,6 +5,7 @@ import dev.daniel.enums.HouseDesign;
 import dev.daniel.enums.SwimmingPool;
 
 public class House {
+
     private int floors;
     private String color;
     private HouseDesign design;
@@ -15,5 +16,71 @@ public class House {
     private Garden garden;
     private int statues;
     private boolean garage;
+
+
+    public House(int floors, String color, HouseDesign design, float area, float price) {
+        this.floors = floors;
+        this.color = color;
+        this.design = design;
+        this.area = area;
+        this.price = price;
+    }
+
+    public void setSwimmingPool(SwimmingPool swimmingPool) {
+        this.swimmingPool = swimmingPool;
+    }
+
+
+    public void setGarden(Garden garden) {
+        this.garden = garden;
+    }
+
+
+    public void setStatues(int statues) {
+        this.statues = statues;
+    }
+
+
+    public void setGarage(boolean garage) {
+        this.garage = garage;
+    }
+
+    public int getFloors() {
+        return floors;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public HouseDesign getDesign() {
+        return design;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public SwimmingPool getSwimmingPool() {
+        return swimmingPool;
+    }
+
+    public Garden getGarden() {
+        return garden;
+    }
+
+    public int getStatues() {
+        return statues;
+    }
+
+    public boolean isGarage() {
+        return garage;
+    }
+
+    
 
 }
