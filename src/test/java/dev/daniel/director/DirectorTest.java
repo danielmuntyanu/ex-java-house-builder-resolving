c54cc879a96b42aa5c51128ecb1d6126ca7d4d5f
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-01 14:45:21 +0200

    test: Director unit test for method constructHouseWithGarage()

--- src/test/java/dev/daniel/director/DirectorTest.java
+++ src/test/java/dev/daniel/director/DirectorTest.java
@@ -1,7 +1,29 @@
 package dev.daniel.director;
 
-// тестирую четыре функции директора, проверяю отличия домов 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import dev.daniel.garage.Garage;
+import dev.daniel.house.House;
+import dev.daniel.house.HouseBuilder;
+import dev.daniel.house.IBuilder;
 
 public class DirectorTest {
+
+    @Test
+    void testDirector_ShouldCreateHouseWithGarage() {
+        Director director = new Director();
+        IBuilder builder = new HouseBuilder();
+
+        House houseWithGarage = director.constructHouseWithGarage(builder);
+
+        assertThat(houseWithGarage, is(instanceOf(House.class)));
+        assertThat(houseWithGarage.getGarage(), is(instanceOf(Garage.class)));
+        assertThat(houseWithGarage.getSwimmingPool(), is(nullValue()));
+        assertThat(houseWithGarage.getGarden(), is(nullValue()));
+        assertThat(houseWithGarage.getStatues(), is(equalTo(0)));
+    }
 
 }
