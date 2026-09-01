e69e564fd495346e8703fa4d7a9220a67f71a6d4
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-01 11:18:27 +0200

    feat: HouseBuilder implementation

--- src/main/java/dev/daniel/house/HouseBuilder.java
+++ src/main/java/dev/daniel/house/HouseBuilder.java
@@ -18,6 +18,56 @@ public class HouseBuilder implements IBuilder {
     private int statues;
     private Garage garage;
 
+
+    @Override
+    public IBuilder setArea(float area) {
+        this.area = area;
+        return this;
+    }
+
+    @Override
+    public IBuilder setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    @Override
+    public IBuilder setDesign(HouseDesign design) {
+        this.design = design;
+        return this;
+    }
+
+    @Override
+    public IBuilder setFloors(int floors) {
+        this.floors = floors;
+        return this;
+    }
+
+    @Override
+    public IBuilder setGarage(Garage garage) {
+        this.garage = garage;
+        return this;
+    }
+
+    @Override
+    public IBuilder setGarden(Garden garden) {
+        this.garden = garden;
+        return this;
+    }
+
+    @Override
+    public IBuilder setStatues(int statues) {
+        this.statues = statues;
+        return this;
+    }
+
+    @Override
+    public IBuilder setSwimmingPool(SwimmingPool swimmingPool) {
+        this.swimmingPool = swimmingPool;
+        return this;
+    }
+
+
     public House getResult() {
         return new House(floors, color, design, area, price, swimmingPool, garden, statues, garage);
     };
